abc174d0cae3ce55288fbc76c420ccbe24156429
Dan Rodrigues <danrodrigues999@gmail.com>
2021-03-15 17:35:48 +0000

    Refactoring detector benchmark for AP computation
    Refactored and restructured detector benchmark. Blocked out sequence of operations for AP computation

--- src/detector_benchmark.py
+++ src/detector_benchmark.py
@@ -9,65 +9,70 @@ import numpy
 from detection import houghDetect
 from functools import partial
 
-# Paths for test images and labels (temporarily hardcoded)
-img_path = "ScaledYOLOv4/vials/test/images/0.jpg"
-label_path = "ScaledYOLOv4/vials/test/labels/0.txt"
+from chrono import Timer
 
-# Read image and resize to 50%
-img = cv2.imread(img_path)
-img = cv2.resize(img, (int(img.shape[1] * 0.5), int(img.shape[0] * 0.5)))
+import glob
+from copy import deepcopy
+
+# Paths for test images and labels
+image_paths = r"ScaledYOLOv4/vials/test/images/"
+label_paths = r"ScaledYOLOv4/vials/test/labels/"
+
+# Find all items at target path
+images = sorted(glob.glob(image_paths + "*.jpg"))
+labels = sorted(glob.glob(label_paths + "*.txt"))
 
 # Define detector(s)
 detector_func = partial(houghDetect, dp = 1.5, minDist = 20, 
-                                    param1 = 27, param2 = 19, 
-                                    minRadius = 12, maxRadius = 15, debug = False)
+                        param1 = 27, param2 = 19, 
+                        minRadius = 12, maxRadius = 15, debug = False)
 
 
-# Perform detection
-ret, bboxes, points = detector_func(img)
-for bbox in bboxes:
+# Dictionary to encode detections
+#Image = image index, Bbox = bbox coordinates, Conf = confidence
+#TP/FP = binary flag indicating TP (1) or FP (0)
+detections = {"image":[], "bbox":[], "conf":[], "TP/FP":[]}
+
+for index, (img_path, label_path) in enumerate(zip(images, labels)):
+
+    # Read image and resize to 50%
+    img = cv2.imread(img_path)
+    img = cv2.resize(img, (int(img.shape[1] * 0.5), int(img.shape[0] * 0.5)))
+
+    gt_bboxes = []
+    # Read contents of GT label data
+    with open(label_path) as file:
+        bboxes = file.readlines()
+        for bbox in bboxes:
+            # Read data and split into separate variables
+            obj_class, x, y, w, h = list(map(float, bbox.rstrip().split(" ")))
+
+            # Undo normalisation of bbox
+            x, w = int(x * img.shape[1]), int(w * img.shape[1])
+            y, h = int(y * img.shape[0]), int(h * img.shape[0])
+
+            # Convert bbox notation to corner points
+            x1, x2 = int(x - w/2), int(x + w/2)
+            y1, y2 = int(y - h/2), int(y + h/2)
+            gt_bboxes.append((x1,y1,x2,y2))
     
-    x1, y1, x2, y2, conf, class_id = bbox
+    # Perform detection
+    with Timer() as timed:
+        ret, bboxes, points = detector_func(img)
+    detection_time = timed.elapsed
+    print("Time elapsed is:", detection_time)
+    
+    for bbox in bboxes:
+        x1, y1, x2, y2, conf, class_id = bbox
+        detections["image"].append(index)
+        detections["bbox"].append((x1,y1,x2,y2))
+        detections["conf"].append(conf)
 
-    # Print coordinate values
-    # print(x1, y1, x2, y2)
+        # Iterate through all gt_bboxes, calculating IoU to identify if TP/FP
 
-    # Draw rectangle in green for detection visualisation
-    cv2.rectangle(img, (x1,y1), (x2,y2), (0,255,0), 2)
+    # Then sort the detections by decreasing confidence
+    # Find the accumulated TPs, FPs
+    # Calculate the precision and recall
 
-
-# Read contents of GT label data
-with open(label_path) as file:
-    bboxes = file.readlines()
-
-
-# Iterate through detections in label file
-for bbox in bboxes:
-
-    # Read data and split into separate variables
-    obj_class, x, y, w, h = list(map(float, bbox.rstrip().split(" ")))
-
-    # Undo normalisation of bbox
-    x = int(x * img.shape[1])
-    w = int(w * img.shape[1])
-    y = int(y * img.shape[0])
-    h = int(h * img.shape[0])
-
-    # Convert bbox notation to corner points
-    x1 = int(x - w/2)
-    x2 = int(x + w/2)
-    y1 = int(y - h/2)
-    y2 = int(y + h/2)
-
-    # Print coordinate values
-    # print(x1, y1, x2, y2)
-
-    # Draw rectangle in red for GT visualisation
-    cv2.rectangle(img, (x1,y1), (x2,y2), (0,0,255), 2)
-
-
-# Show image until keypress
-cv2.imshow("Img", img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
-
+    # Find the 11-point interpolated AP (average precision)
+    # = 1/11(SUM(precision@recall_[0.0,0.1,0.2,...,0.8,0.9,1.0]))
